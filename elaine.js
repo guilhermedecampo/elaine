f555af0410ffb9773edfe2d3daf8932778c1dfc9
Guilherme Decampo <guilherme.decampo@gmail.com>
2014-01-19 00:35:50 -0200

    more changes

---
+++ elaine.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    path = require('path'),
+    http = require('http');
+
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/views');
+    app.engine('html', require('ejs').renderFile);
+});
+
+
+app.get('/', function (req, res){
+  res.render('index.html');
+});
+
+app.use(function (err, req, res, next){
+      // treat as 404
+      if (~err.message.indexOf('not found')) return next();
+
+      // log it
+      console.error(err.stack);
+
+      // error page
+      res.status(500).render('index.html');
+
+});
+
+    // assume 404 since no middleware responded
+app.use(function (req, res, next){
+    res.status(404).render('index.html');
+
+});
+
+app.listen(1355);
